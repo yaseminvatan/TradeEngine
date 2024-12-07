fc81b3484d1af27998a20462124ba9e01742a83e
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-06 22:11:07 -0800

    bug fixed: initialized Product tags with related fields.

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -13,8 +13,13 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    tag_name: { 
-      type: DataTypes.STRING 
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: { model: 'product', key: 'id' },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: { model: 'tag', key: 'id' },
     },
   },
   {
