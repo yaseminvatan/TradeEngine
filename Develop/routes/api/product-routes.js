b1ec33eab9ea57f1245fadaca53cad81386f3259
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-07 07:15:14 -0800

    Add endpoint to create a new product with optional tag associations

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -43,7 +43,7 @@ router.get('/:id', async(req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -52,6 +52,23 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+    try {
+      const product = await Product.create(req.body);
+  
+      // If product tags are provided, create pairings to bulk create in ProductTag
+      if (req.body.tagIds && req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+          return { product_id: product.id, tag_id };
+        });
+        await ProductTag.bulkCreate(productTagIdArr);
+      }
+  
+      res.status(200).json(product);
+    } catch (err) {
+      console.error(err);
+      res.status(400).json(err);
+    }
+  });
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -72,8 +89,6 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
-
 // update product
 router.put('/:id', (req, res) => {
   // update product data
