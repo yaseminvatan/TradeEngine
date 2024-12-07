dcbe29cfc179a81a0493206d1fd9b581fc385694
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-06 22:53:37 -0800

    Add endpoint to fetch a single product by ID with associated category and tags

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -21,9 +21,25 @@ router.get('/', async(req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [
+        { model: Category },
+        { model: Tag, through: ProductTag },
+      ],
+    });
+    if (!product) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
